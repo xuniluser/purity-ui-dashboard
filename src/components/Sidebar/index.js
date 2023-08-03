0ccbb57417cfc96f40ed14efde0812049e0a7430
xuniluser <xuniluser@gmail.com>
2023-08-04 02:41:42 +0800

    Changed Text to "STUDENT MONITORING Dashboard"

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -48,7 +48,7 @@ function Sidebar(props) {
           borderRadius={sidebarRadius}
         >
           <SidebarContent routes={routes}
-        logoText={"PURITY UI DASHBOARD"}
+        logoText={"STUDENT MONITORING Dashboard"}
         display="none"
         sidebarVariant={sidebarVariant}
         />
